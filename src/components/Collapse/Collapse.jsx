c017c9249aad4e3ad63f4ec558fc8ddc9113eb7e
Alessandra Gomes de Lima <alessandragdlima1@gmail.com>
2024-02-11 20:40:50 +0100

    Arrow Changes

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -2,23 +2,23 @@ import React, { useState } from "react";
 import "./Collapse.scss";
 import arrow from "../../assets/images/Fleche.svg";
 
-function Collapse({titre, description}) {
+function Collapse({ titre, description }) {
+  const [isOpen, setIsOpen] = useState(false);
 
-    const [Open, IsOpen] = useState(false);
-
-    return(
-        <div className="collapse" id={`collapse-${titre}`}>
-            <div className="CollapseHeader">
-                <div className="CollapseTitle">{titre}</div>
-                <span className={`arrow-collapse ${Open}`} onClick={() => IsOpen(!Open)}>
-                    <img src={arrow} alt="Ouvrir"/>
-                </span>
-            </div>
-            {
-                Open && <div className="CollapseDescription">{description}</div>
-            }
-        </div>
-    );
+  return (
+    <div className="collapse" id={`collapse-${titre}`}>
+      <div className="CollapseHeader">
+        <div className="CollapseTitle">{titre}</div>
+        <span
+          className={`arrow-collapse ${isOpen ? '' : 'open'}`}
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <img src={arrow} alt="Ouvrir" />
+        </span>
+      </div>
+      {isOpen && <div className="CollapseDescription">{description}</div>}
+    </div>
+  );
 }
 
 export default Collapse;
